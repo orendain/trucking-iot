38405fd7ab1def4582e3ca2a5f01f5a89efbd4af
Edgar Orendain <edgar@orendainx.com>
2017-01-20 04:30:01 -0800

    Dependency and build.sbt cleanup

--- build.sbt
+++ build.sbt
@@ -87,84 +87,36 @@ lazy val nifiBundle = (project in file("trucking-nifi-bundle"))
 
 
 
-lazy val ngVersion = "2.4.3"
+
 lazy val webAppBackend = (project in file("trucking-web-app/backend"))
+  .dependsOn(topology)
   .settings(
     commonSettings,
     name := "trucking-web-app-backend",
-  scalaJSProjects := Seq(webAppFrontend),
+    scalaJSProjects := Seq(webAppFrontend),
 
+    unmanagedResources in Assets ++= Seq(
+      baseDirectory.value / "../frontend/target/scala-2.11/trucking-web-app-frontend-sjsx.js",
+      baseDirectory.value / "../frontend/target/scala-2.11/trucking-web-app-frontend-fastop.js"
+    ),
+    unmanagedResourceDirectories in Assets += baseDirectory.value / "../frontend/src/main/resources",
 
-  unmanagedResources in Assets ++= Seq(
-    baseDirectory.value / "../frontend/target/scala-2.11/trucking-web-app-frontend-sjsx.js",
-    baseDirectory.value / "../frontend/target/scala-2.11/trucking-web-app-frontend-fastop.js"
-  ),
-  unmanagedResourceDirectories in Assets += baseDirectory.value / "../frontend/src/main/resources",
-
-  pipelineStages in Assets := Seq(scalaJSPipeline),
-  //pipelineStages := Seq(digest, gzip),
-  // triggers scalaJSPipeline when using compile or continuous compilation
-  compile in Compile <<= (compile in Compile) dependsOn scalaJSPipeline,
-  //compile in Compile <<= (compile in Compile) dependsOn (fastOptJS in (map, Compile)),
-  resolvers += Resolver.mavenLocal,
-  libraryDependencies ++= Seq(
-    filters,
-    cache,
-    ws,
-    "com.vmunier" %% "scalajs-scripts" % "1.0.0",
-    "org.webjars" %% "webjars-play" % "2.5.0",
-
-    //angular2 dependencies
-    "org.webjars.npm" % "angular__common" % ngVersion,
-    "org.webjars.npm" % "angular__compiler" % ngVersion,
-    "org.webjars.npm" % "angular__core" % ngVersion,
-    "org.webjars.npm" % "angular__http" % ngVersion,
-    "org.webjars.npm" % "angular__forms" % ngVersion,
-    "org.webjars.npm" % "angular__platform-browser-dynamic" % ngVersion,
-    "org.webjars.npm" % "angular__platform-browser" % ngVersion,
-    "org.webjars.npm" % "angular__router" % "3.4.3",
-
-    "org.webjars.npm" % "systemjs" % "0.19.41",
-    "org.webjars.npm" % "rxjs" % "5.0.3",
-    "org.webjars.npm" % "reflect-metadata" % "0.1.9",
-    "org.webjars.npm" % "zone.js" % "0.6.26",
-    "org.webjars.npm" % "core-js" % "2.4.1",
-    "org.webjars.npm" % "symbol-observable" % "1.0.4",
-
-    "org.webjars.bower" % "compass-mixins" % "0.12.10",
-    "org.webjars.bower" % "bootstrap-sass" % "3.3.6",
-
-
-    "com.orendainx.hortonworks" %% "trucking-topology" % "0.3.2",
-    "org.apache.storm" % "storm-core" % "1.0.2"
-  ),
-
-  scalacOptions += "-Yresolve-term-conflict:package",
-
-  promptTheme := ScalapenosTheme,
-  shellPrompt := (state ⇒ promptTheme.value.render(state))
-).enablePlugins(PlayScala)
-
+    pipelineStages in Assets := Seq(scalaJSPipeline),
+    //pipelineStages := Seq(digest, gzip),
+    compile in Compile <<= (compile in Compile) dependsOn scalaJSPipeline,
+    resolvers += Resolver.mavenLocal, // For topology > schema-registry-serdes
+    libraryDependencies ++= (Seq(filters, cache, ws) ++ Dependencies.webAppBackendDeps),
+    scalacOptions += "-Yresolve-term-conflict:package",
+    shellPrompt := (state ⇒ promptTheme.value.render(state)) // Override Play's Sbt prompt
+  ).enablePlugins(PlayScala)
 
 lazy val webAppFrontend = (project in file("trucking-web-app/frontend"))
+  .dependsOn(common)
   .settings(
     commonSettings,
-  name := "trucking-web-app-frontend",
-  //persistLauncher := true,
-  //persistLauncher in Test := false,
-  resolvers += "jitpack" at "https://jitpack.io",
-  libraryDependencies ++= Seq(
-    //"org.scala-js" %%% "scalajs-dom" % "0.9.1",
-    "com.orendainx.hortonworks" %% "trucking-common" % "0.3.2",
-    "com.github.fancellu.scalajs-leaflet" % "scalajs-leaflet_sjs0.6_2.11" % "v0.1",
-    "io.github.cquiroz" %%% "scala-java-time" % "2.0.0-M6"
-  ),
-  jsDependencies ++= Seq(
-    "org.webjars.npm" % "leaflet" % "1.0.2" / "leaflet.js" commonJSName "Leaflet"
-  ),
-  //ngBootstrap := Some("Frontend") //qualified name (including packages) of Scala class called NAME_OF_THE_MODULE_TO_BOOTSTRAP
-  ngBootstrap := Some("com.orendainx.hortonworks.trucking.webapp.AppModule") //qualified name (including packages) of Scala class called NAME_OF_THE_MODULE_TO_BOOTSTRAP
-).enablePlugins(ScalaJSPlugin, ScalaJSWeb, Angulate2Plugin)
-
-// loads the server project at sbt startup
-//onLoad in Global := (Command.process("project server", _: State)) compose (onLoad in Global).value
+    name := "trucking-web-app-frontend",
+    resolvers += "jitpack" at "https://jitpack.io", // For scalajs-leaflet
+    libraryDependencies ++= Seq("io.github.cquiroz" %%% "scala-java-time" % "2.0.0-M6") ++ Dependencies.webAppFrontendDeps,
+    jsDependencies ++= Seq("org.webjars.npm" % "leaflet" % "1.0.2" / "leaflet.js" commonJSName "Leaflet"),
+    ngBootstrap := Some("com.orendainx.hortonworks.trucking.webapp.AppModule")
+  ).enablePlugins(ScalaJSPlugin, ScalaJSWeb, Angulate2Plugin)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,22 +2,25 @@ import sbt._
 
 object Dependencies {
 
+  lazy val ngVersion = "2.4.3"
+
   // Scala libraries
-  val config = "com.typesafe" % "config" % "1.3.1"
-  val betterFiles = "com.github.pathikrit" %% "better-files" % "2.16.0"
-  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
-  val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.4"
+  lazy val config = "com.typesafe" % "config" % "1.3.1"
+  lazy val betterFiles = "com.github.pathikrit" %% "better-files" % "2.16.0"
+  lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
+  lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.4"
 
   // Logging backends
-  val logback = "ch.qos.logback" % "logback-classic" % "1.1.8"
+  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.1.8"
 
   // Akka libraries
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.4.14"
+  lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.4.14"
 
   // Apache libraries
-  val nifiStormSpout = "org.apache.nifi" % "nifi-storm-spout" % "1.1.0"
-  val stormCore = "org.apache.storm" % "storm-core" % "1.0.2" % "provided"
-  val schemaRegistrySerdes = ("com.hortonworks.registries" % "schema-registry-serdes" % "0.1.0-SNAPSHOT")
+  lazy val nifiStormSpout = "org.apache.nifi" % "nifi-storm-spout" % "1.1.0"
+  lazy val stormCore = "org.apache.storm" % "storm-core" % "1.0.2"
+  lazy val stormCoreProvided = "org.apache.storm" % "storm-core" % "1.0.2" % "provided"
+  lazy val schemaRegistrySerdes = ("com.hortonworks.registries" % "schema-registry-serdes" % "0.1.0-SNAPSHOT")
   //val schemaRegistrySerdes = ("com.hortonworks.registries" % "schema-registry-serdes" % "0.0.1.2.2.0.0-5" from "http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/schema-registry-serdes/0.0.1.2.2.0.0-5/schema-registry-serdes-0.0.1.2.2.0.0-5.jar")
     .exclude("org.slf4j", "log4j-over-slf4j")
     .exclude("commons-beanutils", "commons-beanutils-core")
@@ -25,20 +28,52 @@ object Dependencies {
     //.exclude("commons-beanutils", "commons-beanutils-core")
     //.exclude("commons-collections", "commons-collections")
     //.exclude("commons-beanutils", "commons-beanutils")
-  val schemaRegistryClient = ("com.hortonworks.registries" % "schema-registry-client" % "0.0.1.2.2.0.0-5" from "http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/schema-registry-client/0.0.1.2.2.0.0-5/schema-registry-client-0.0.1.2.2.0.0-5.jar")
+  lazy val schemaRegistryClient = ("com.hortonworks.registries" % "schema-registry-client" % "0.0.1.2.2.0.0-5" from "http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/schema-registry-client/0.0.1.2.2.0.0-5/schema-registry-client-0.0.1.2.2.0.0-5.jar")
       .exclude("org.slf4j", "log4j-over-slf4j")
       .exclude("commons-beanutils", "commons-beanutils-core")
-  val schemaRegistryCommon = ("com.hortonworks.registries" % "schema-registry-common" % "0.0.1.2.2.0.0-5" from "http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/schema-registry-common/0.0.1.2.2.0.0-5/schema-registry-common-0.0.1.2.2.0.0-5.jar")
-  val glass = "org.glassfish.jersey.core" % "jersey-client" % "2.25"
-  val schemaRegistryCore = ("com.hortonworks.registries" % "schema-registry-core" % "0.0.1.2.2.0.0-5" from "http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/schema-registry-core/0.0.1.2.2.0.0-5/schema-registry-core-0.0.1.2.2.0.0-5.jar")
+  lazy val schemaRegistryCommon = ("com.hortonworks.registries" % "schema-registry-common" % "0.0.1.2.2.0.0-5" from "http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/schema-registry-common/0.0.1.2.2.0.0-5/schema-registry-common-0.0.1.2.2.0.0-5.jar")
+  lazy val glass = "org.glassfish.jersey.core" % "jersey-client" % "2.25"
+  lazy val schemaRegistryCore = ("com.hortonworks.registries" % "schema-registry-core" % "0.0.1.2.2.0.0-5" from "http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/schema-registry-core/0.0.1.2.2.0.0-5/schema-registry-core-0.0.1.2.2.0.0-5.jar")
     .exclude("org.slf4j", "slf4j-api")
     .exclude("org.slf4j", "log4j-over-slf4j")
     .exclude("commons-beanutils", "commons-beanutils-core")
+
+  val angular2Deps = Seq(
+    "org.webjars.npm" % "angular__common" % ngVersion,
+    "org.webjars.npm" % "angular__compiler" % ngVersion,
+    "org.webjars.npm" % "angular__core" % ngVersion,
+    "org.webjars.npm" % "angular__http" % ngVersion,
+    "org.webjars.npm" % "angular__forms" % ngVersion,
+    "org.webjars.npm" % "angular__platform-browser-dynamic" % ngVersion,
+    "org.webjars.npm" % "angular__platform-browser" % ngVersion,
+    "org.webjars.npm" % "angular__router" % "3.4.3",
+    "org.webjars.npm" % "systemjs" % "0.19.41",
+    "org.webjars.npm" % "rxjs" % "5.0.3",
+    "org.webjars.npm" % "reflect-metadata" % "0.1.9",
+    "org.webjars.npm" % "zone.js" % "0.6.26",
+    "org.webjars.npm" % "core-js" % "2.4.1",
+    "org.webjars.npm" % "symbol-observable" % "1.0.4")
+
+  //val playDeps = Seq(filters, cache, ws)
+  val scalaJsScripts = "com.vmunier" %% "scalajs-scripts" % "1.0.0"
+  val webjars = "org.webjars" %% "webjars-play" % "2.5.0"
+
+
+  // Web dependencies
+  val compass = "org.webjars.bower" % "compass-mixins" % "0.12.10"
+  val bootstrap = "org.webjars.bower" % "bootstrap-sass" % "3.3.6"
+
+  val leaflet = "com.github.fancellu.scalajs-leaflet" % "scalajs-leaflet_sjs0.6_2.11" % "v0.1"
+  //val scalaTime = "io.github.cquiroz" %%% "scala-java-time" % "2.0.0-M6"
 
   // Projects
   val simulatorDeps = Seq(akkaActor, config, betterFiles, scalaLogging, logback)
   val enrichmentDeps = Seq(scalaCsv, config, betterFiles)
   val schemaRegistrarDeps = Seq(schemaRegistrySerdes, config, scalaLogging, logback)
-  val topologyDeps = Seq(nifiStormSpout, stormCore, glass, schemaRegistrySerdes, config, betterFiles, scalaLogging)
+  val topologyDeps = Seq(nifiStormSpout, stormCoreProvided, glass, schemaRegistrySerdes, config, betterFiles, scalaLogging)
   //val topologyDeps = Seq(nifiStormSpout, stormCore, glass, schemaRegistrySerdes, schemaRegistryClient, schemaRegistryCommon, schemaRegistryCore, config, betterFiles, scalaLogging)
+
+  val webAppBackendDeps = angular2Deps ++ Seq(scalaJsScripts, webjars, compass, bootstrap, stormCore)
+  val webAppFrontendDeps = Seq(leaflet)
+
 }
