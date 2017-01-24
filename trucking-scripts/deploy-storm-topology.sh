1fcf34adb3cce4ed24a5ec291d55d19af70c659b
Edgar Orendain <edgar@orendainx.com>
2017-01-23 19:44:22 -0800

    Script changes

--- trucking-scripts/deploy-storm-topology.sh
+++ trucking-scripts/deploy-storm-topology.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sbt topology/assembly
-storm jar ../trucking-topology/target/scala-2.11/trucking-topology-assembly-0.3.2.jar com.orendainx.hortonworks.trucking.topology.TruckingTopology
+PROJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+cd $PROJ_DIR && sbt topology/assembly
+storm jar $PROJ_DIR/trucking-topology/target/scala-2.11/trucking-topology-assembly-0.3.2.jar com.orendainx.hortonworks.trucking.topology.TruckingTopology
--- trucking-scripts/setup-schema-registry.sh
+++ trucking-scripts/setup-schema-registry.sh
@@ -4,4 +4,5 @@
 nohup /usr/hdf/2.1.0.0-164/registry/bin/registry-server-start.sh /usr/hdf/2.1.0.0-164/registry/conf/registry-dev.yaml &
 
 # Run the module to registry the project's schema
-sbt schemaRegistrar/run
+PROJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+cd $PROJ_DIR && sbt schemaRegistrar/run
