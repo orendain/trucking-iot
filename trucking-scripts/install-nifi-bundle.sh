3636137e90315cfaf264900ffbdc408a6bf8f8f2
Edgar Orendain <edgar@orendainx.com>
2017-01-23 19:33:20 -0800

    Updated install-nifi-bundle.sh

--- trucking-scripts/install-nifi-bundle.sh
+++ trucking-scripts/install-nifi-bundle.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sbt nifiBundle/compile
-cp ../trucking-nifi-bundle/nifi-trucking-nar/target/nifi-trucking-nar-0.3.2.nar /usr/hdf/current/nifi/lib/
+#sbt nifiBundle/compile
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+cp $DIR/trucking-nifi-bundle/nifi-trucking-nar/target/nifi-trucking-nar-0.3.2.nar /usr/hdf/current/nifi/lib/
