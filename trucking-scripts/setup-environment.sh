69b151ab996a96ac7866e71dd9e2cca9945d9092
Edgar Orendain <edgar@orendainx.com>
2017-01-26 14:46:03 -0800

    Fixed paths in scripts.  Additionally, intentionally not included required files - will include a release of those soon

--- trucking-scripts/setup-environment.sh
+++ trucking-scripts/setup-environment.sh
@@ -31,14 +31,14 @@ cd registry
 mvn install -DskipTests
 
 # Install Schema Registry
-rpm -Uvh $PROJ_DIR/resources/trucking-scripts/registry_2_1_0_0_164-0.0.1.2.1.0.0-164.el6.noarch.rpm
+rpm -Uvh $PROJ_DIR/trucking-scripts/resources/registry_2_1_0_0_164-0.0.1.2.1.0.0-164.el6.noarch.rpm
 cp /usr/hdf/2.1.0.0-164/etc/registry/conf.dist/* /usr/hdf/2.1.0.0-164/registry/conf/
 
 # Replace instances of port 9090 to 8090 in config
 perl -pi -e 's/9090/8080/g' /usr/hdf/2.1.0.0-164/registry/conf/registry-dev.yaml
 
 # Install Schema Registry nar file
-cp $PROJ_DIR/resources/nifi-registry-nar-0.0.1-SNAPSHOT.nar /usr/hdf/current/nifi/lib
+cp $PROJ_DIR/trucking-scripts/resources/nifi-registry-nar-0.0.1-SNAPSHOT.nar /usr/hdf/current/nifi/lib
 
 # Create necessary Kafka topics
 /usr/hdf/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partition 1 --topic trucking.data.truckandtraffic
