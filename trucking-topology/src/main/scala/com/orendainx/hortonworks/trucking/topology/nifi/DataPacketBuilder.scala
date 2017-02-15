dab2a59921d0fa0fc9fc17dd7924a61f0e36ef1f
Edgar Orendain <edgar@orendainx.com>
2017-02-15 06:34:37 -0800

    DataPacketBuilder now handles cases when the incoming tuple is from a join bolt or windowing bolt

--- trucking-topology/src/main/scala/com/orendainx/hortonworks/trucking/topology/nifi/DataPacketBuilder.scala
+++ trucking-topology/src/main/scala/com/orendainx/hortonworks/trucking/topology/nifi/DataPacketBuilder.scala
@@ -2,7 +2,7 @@ package com.orendainx.hortonworks.trucking.topology.nifi
 
 import java.nio.charset.StandardCharsets
 
-import com.orendainx.hortonworks.trucking.common.models.WindowedDriverStats
+import com.orendainx.hortonworks.trucking.common.models.{EnrichedTruckAndTrafficData, WindowedDriverStats}
 import com.typesafe.scalalogging.Logger
 import org.apache.nifi.storm.{NiFiDataPacket, NiFiDataPacketBuilder, StandardNiFiDataPacket}
 import org.apache.storm.tuple.Tuple
@@ -19,8 +19,13 @@ class DataPacketBuilder extends NiFiDataPacketBuilder with Serializable {
   override def createNiFiDataPacket(tuple: Tuple): NiFiDataPacket = {
     val newAttributes = Map("processed" -> "true")
 
+    val outData = tuple.getSourceComponent match {
+      case "joinedData" => tuple.getValue(0).asInstanceOf[EnrichedTruckAndTrafficData].toCSV.getBytes(StandardCharsets.UTF_8)
+      case "windowedDriverStats" => tuple.getValue(0).asInstanceOf[WindowedDriverStats].toCSV.getBytes(StandardCharsets.UTF_8)
+    }
+
     // When reading in non-serialized data
-    new StandardNiFiDataPacket(tuple.getValue(0).asInstanceOf[WindowedDriverStats].toCSV.getBytes(StandardCharsets.UTF_8), newAttributes)
+    new StandardNiFiDataPacket(outData, newAttributes)
 
     // When reading from SerializerBolt
     //new StandardNiFiDataPacket(tuple.getValueByField("serializedData").asInstanceOf[Array[Byte]], newAttributes)
