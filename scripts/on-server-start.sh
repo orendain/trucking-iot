8c27ac8855f6dce38bfc145a1f66c6423546b612
Edgar Orendain <edgar@orendainx.com>
2017-02-16 14:23:35 -0800

    Script documentation via echos

--- scripts/on-server-start.sh
+++ scripts/on-server-start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Find the registry webservice binary and restart the service
+echo "Restarting the registry service"
 registry=$(find / -type f -wholename '/usr/hd*/registry' -print -quit)
-echo "Found Registry binaries at $registry"
-
 $registry stop
 $registry clean
 $registry start
--- scripts/run-project.sh
+++ scripts/run-project.sh
@@ -3,21 +3,23 @@
 # Variables
 projVer="0.3.2"
 projDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
-nifiLibs=($(find / -path "/usr/*/nifi/lib"))
-nifiSh=($(find / -type f -name "nifi.sh"))
+nifiLibDir=$(find / -type d -path "/usr/*/nifi/lib" -print -quit)
+nifiSh=$(find / -type f -wholename "/usr/*/nifi.sh" -print -quit)
 
 # Jump to the project directory for executing SBT commands
 cd $projDir
 
 # NiFI: Compile the trucking-nifi-bundle project, generating a nar file to copy to the NiFi lib directory.
-$nifiSh stop
+echo "Building and installing a NiFi nar to NiFi.  NiFi will be restarted"
 sbt nifiBundle/compile
-cp $projDir/trucking-nifi-bundle/nifi-trucking-nar/target/nifi-trucking-nar-${projVer}.nar ${nifiLibs[0]}
-$nifiSh start
+cp -f $projDir/trucking-nifi-bundle/nifi-trucking-nar/target/nifi-trucking-nar-$projVer.nar $nifiLibDir
+$nifiSh restart
 
 # Schema Registry: Register the project schemas with the registry
+echo "Registering schema with Schema Registry"
 sbt schemaRegistrar/run
 
 # Storm: Build and deploy the Storm topology
+echo "Building and deploying a Storm topology"
 sbt topology/assembly
-storm jar $projDir/trucking-topology/target/scala-2.11/trucking-topology-assembly-${projVer}.jar com.orendainx.hortonworks.trucking.topology.TruckingTopology
+storm jar $projDir/trucking-topology/target/scala-2.11/trucking-topology-assembly-$projVer.jar com.orendainx.hortonworks.trucking.topology.TruckingTopology
