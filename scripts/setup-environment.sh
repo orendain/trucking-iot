f50c8dda75fbc4c34c3a54fd8da2b638dd3176fa
Edgar Orendain <edgar@orendainx.com>
2017-02-23 12:57:09 -0800

    Include necessary Kafka topic creation in setup-environment.sh

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -17,6 +17,8 @@ yum install -y apache-maven
 echo "Starting Kafka and creating Kafka topics"
 $(find / -type f -wholename '/usr/hd*/kafka' -print -quit) start
 kafkaTopicsSh=$(find / -type f -wholename '/usr/hd*/kafka-topics.sh' -print -quit)
+$kafkaTopicsSh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partition 1 --topic trucking_data_truck
+$kafkaTopicsSh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partition 1 --topic trucking_data_traffic
 $kafkaTopicsSh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partition 1 --topic trucking_data_joined
 $kafkaTopicsSh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partition 1 --topic trucking_data_driverstats
 
