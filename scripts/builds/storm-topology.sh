22d074d29a46383a9a9b0b77909348e580d9f1e1
Edgar Orendain <edgar@orendainx.com>
2017-03-05 23:24:29 -0800

    Fix storm deployment script

--- scripts/builds/storm-topology.sh
+++ scripts/builds/storm-topology.sh
@@ -9,4 +9,4 @@ cd $projDir
 
 echo "Building and deploying the Storm topology"
 sbt stormTopology/assembly
-storm jar $projDir/trucking-storm-topology/target/scala-2.11/trucking-storm-topology-assembly-$projVer.jar com.orendainx.hortonworks.trucking.topology.topologies.KafkaToKafka
+storm jar $projDir/trucking-storm-topology/target/scala-2.11/trucking-storm-topology-assembly-$projVer.jar com.orendainx.hortonworks.trucking.storm.topologies.KafkaToKafka
