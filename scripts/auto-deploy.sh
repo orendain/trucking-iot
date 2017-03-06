0c0461625f0f8ab9980bb0a7a9b70750a6acb4fd
Edgar Orendain <edgar@orendainx.com>
2017-03-05 22:42:45 -0800

    Update auto-deploy.sh

--- scripts/auto-deploy.sh
+++ scripts/auto-deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+scriptDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
 # Setup the environment before building project components
-./setup-environment.sh
+$scriptDir/setup-environment.sh
 
 # Find the registry webservice and start it
-./registry-restart.sh
+$scriptDir/registry-restart.sh
 
 # Build and install the trucking-nifi-bundle project, generating a NiFi nar for use.
-./builds/nifi-bundle.sh
+$scriptDir/builds/nifi-bundle.sh
 
 # Build and run the trucking-schema-registrar project, registering schema with the registry
-./builds/schema-registrar.sh
+$scriptDir/builds/schema-registrar.sh
 
 # Build and deploy the Storm topology
-./builds/storm-topology.sh
+$scriptDir/builds/storm-topology.sh
 
