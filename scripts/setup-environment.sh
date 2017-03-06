2fb0d0b516ccbe3f638b09e4dd010f6d360a54e1
Edgar Orendain <edgar@orendainx.com>
2017-03-05 22:52:22 -0800

    Update SBT log level and update docs for scripts

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 #
-# Note: This script assumes that Ambari, NiFi and Kafka are up and running at this point.
+# Note: This script assumes that Ambari and NiFi and are up and running at this point.
+# It also assumes an Ambari username/pass of admin/admin and that it's running on port 8080 with hostname "sandbox.hortonworks.com"
 #
 
 # Variables
 projDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 
-# Set nifi.remote.input.socket.port via Ambari
+echo "Setting nifi.remote.input.socket.port to correct value via Ambari"
 /var/lib/ambari-server/resources/scripts/configs.py admin admin 8080 http set sandbox.hortonworks.com Sandbox nifi-properties nifi.remote.input.socket.port 15000
 
-# Stop NiFi via Ambari
+echo "Stopping NiFi via Ambari"
 curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context": "Stop NIFi"}, "ServiceInfo": {"state": "INSTALLED"}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/NIFI
 
-# Install SBT if missing
 echo "Checking for SBT, installing if missing"
 curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
 yum -y install sbt
 
-# Install Maven if missing
 echo "Checking for Maven, installing if missing"
 wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 yum install -y apache-maven
 
-# Use kafka-topics.sh to create necessary topics
 echo "Starting Kafka and creating Kafka topics"
 $(find / -type f -wholename '/usr/hd*/kafka' -print -quit) start
 kafkaTopicsSh=$(find / -type f -wholename '/usr/hd*/kafka-topics.sh' -print -quit)
@@ -32,8 +30,7 @@ $kafkaTopicsSh --create --zookeeper sandbox.hortonworks.com:2181 --replication-f
 $kafkaTopicsSh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partition 10 --topic trucking_data_joined
 $kafkaTopicsSh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partition 10 --topic trucking_data_driverstats
 
-# Move NiFi flow into proper location
-echo "Importing NiFi template.  Existing flow is renamed to flow.xml.gz.bak"
+echo "Importing NiFi flow.  Existing flow is renamed to flow.xml.gz.bak"
 mv /var/lib/nifi/conf/flow.xml.gz /var/lib/nifi/conf/flow.xml.gz.bak
 cp -f $projDir/trucking-nifi-templates/flows/kafka-to-kafka.xml.gz /var/lib/nifi/conf/flow.xml.gz
 
