8786f4df69506c2a45923487b5e023b109224047
Edgar Orendain <edgar@orendainx.com>
2017-05-07 23:48:48 -0700

    Added set-hostname.sh script

---
+++ scripts/set-hostname.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+projDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
+
+if [ $# -eq 0 ]
+  then
+    echo "Pass in the hostname to set"
+else
+  for dir in "scripts" "trucking-schema-registrar" "trucking-simulator" "trucking-nifi-templates" "trucking-storm-topology" "trucking-storm-topology-java" "trucking-web-application"
+  do
+    find $projDir/$dir -type f -exec sed -i '' "s/sandbox.hortonworks.com/$1/g" {} \;
+  done
+fi
