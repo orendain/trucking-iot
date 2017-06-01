74e410ab078d1ab82b1c82d2cca1799107ec79d9
Edgar Orendain <edgar@orendainx.com>
2017-05-31 19:05:06 -0700

    script updates

--- scripts/setup-frontend.sh
+++ scripts/setup-frontend.sh
@@ -17,13 +17,17 @@ sed -i "s/kafkabroker1.kafka-app-2.root.hwx.site:9092/$kafkaBootstrapServer/g" /
 # Set frontend properties via container-id
 containerId=$(get_property trucking-web-application.frontend.container-uri)
 containerUri=$(echo $containerId | sed -e 's/container/ctr/g' -e 's/_/-/g')
-echo "Container uri set to: $websocketUri"
+echo "Container uri set to: $containerUri"
 sed -i "s/localhost:15100/$containerUri/g" /trucking-iot/trucking-web-application/frontend/src/main/scala/com/orendainx/hortonworks/trucking/webapplication/WebSocketService.scala
 
 # Set frontend properties
 #websocketUri=$(get_property trucking-web-application.frontend.kafka.websocket-uri)
 #echo "Websocket uri set to: $websocketUri"
 #sed -i "s/localhost:15100/$websocketUri/g" /trucking-iot/trucking-web-application/frontend/src/main/scala/com/orendainx/hortonworks/trucking/webapplication/WebSocketService.scala
+
+# Enter base directory
+#cd $projDir
+cd "/trucking-iot"
 
 # Clean up pre-compiled frontend files
 sbt "project webApplicationFrontend" "clean"
