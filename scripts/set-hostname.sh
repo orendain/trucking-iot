ebf30ddaeb7b0bcc60a9f0039419cec3b4381176
Edgar Orendain <edgar@orendainx.com>
2017-06-11 05:08:30 -0700

    Improve set-hostname script

--- scripts/set-hostname.sh
+++ scripts/set-hostname.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]
   then
     echo "Pass in the hostname to set"
 else
-  for dir in "scripts" "trucking-schema-registrar" "trucking-simulator" "trucking-nifi-templates" "trucking-storm-topology" "trucking-storm-topology-java" "trucking-web-application"
+  for dir in "scripts" "trucking-schema-registrar/src" "trucking-simulator/src" "trucking-nifi-templates" "trucking-storm-topology/src" "trucking-storm-topology-java/src" "trucking-web-application/backend/conf" "trucking-web-application/frontend/src"
   do
     find $projDir/$dir -type f -exec sed -i '' -e "s/sandbox-hdf.hortonworks.com/$1/g" {} \;
   done
