883c3c21c39bb1c894b2b1b9db7098b826b0d4fa
Edgar Orendain <edgar@orendainx.com>
2017-07-24 04:47:35 -0700

    Default topology change and config changes

--- scripts/builds/storm-topology.sh
+++ scripts/builds/storm-topology.sh
@@ -9,8 +9,8 @@ projVer=$(cat version.sbt | grep '".*"' -o | sed 's/"//g')
 echo "Building and deploying the Storm topology"
 
 # This is the Scala version of the topology
-#sbt stormTopology/assembly
-#storm jar $projDir/trucking-storm-topology/target/scala-2.11/trucking-storm-topology-assembly-$projVer.jar com.orendainx.hortonworks.trucking.storm.topologies.KafkaToKafkaWithSchema
+sbt stormTopology/assembly
+storm jar $projDir/trucking-storm-topology/target/scala-2.11/trucking-storm-topology-assembly-$projVer.jar com.orendainx.hortonworks.trucking.storm.topologies.KafkaToKafka
 
 # Valid, built-in options for the topology (last argument) above are:
 #
@@ -20,6 +20,6 @@ echo "Building and deploying the Storm topology"
 # com.orendainx.hortonworks.trucking.storm.topologies.KafkaToKafkaWithSchema
 
 # This is the Java version of the topology
-sbt stormTopologyJava/assembly
-storm jar $projDir/trucking-storm-topology-java/target/scala-2.11/trucking-storm-topology-java-assembly-$projVer.jar com.orendainx.hortonworks.trucking.storm.java.topologies.KafkaToKafka
+#sbt stormTopologyJava/assembly
+#storm jar $projDir/trucking-storm-topology-java/target/scala-2.11/trucking-storm-topology-java-assembly-$projVer.jar com.orendainx.hortonworks.trucking.storm.java.topologies.KafkaToKafka
 
