b9d576ab21f3ce8b74081be2fff7669fa91abd78
Edgar Orendain <edgar@orendainx.com>
2017-10-21 00:58:07 -0700

    Start NiFi after building the NiFi bundle

--- scripts/builds/nifi-bundle.sh
+++ scripts/builds/nifi-bundle.sh
@@ -7,9 +7,12 @@ cd $projDir
 # Variables
 projVer=$(cat version.sbt | grep '".*"' -o | sed 's/"//g')
 nifiLibDir=$(find / -type d -path "/usr/*/nifi/lib" -print -quit 2> /dev/null)
-nifiSh=$(find / -type f -wholename "/usr/*/nifi.sh" -print -quit 2> /dev/null)
 
 echo "Building the trucking-nifi-bundle project and installing the compiled NiFi nar to NiFi.  NiFi will be restarted"
 sbt nifiBundle/compile
 cp -f $projDir/trucking-nifi-bundle/nifi-trucking-nar/target/nifi-trucking-nar-$projVer.nar $nifiLibDir
-$nifiSh restart
+
+echo "Starting NiFi via Ambari"
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context": "Start NIFi"}, "ServiceInfo": {"state": "STARTED"}}' http://sandbox-hdf.hortonworks.com:8080/api/v1/clusters/Sandbox/services/NIFI | python $projDir/scripts/wait-until-done.py
+#nifiSh=$(find / -type f -wholename "/usr/*/nifi.sh" -print -quit 2> /dev/null)
+#$nifiSh restart
